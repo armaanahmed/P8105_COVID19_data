104798d9c09a2f884951ef2481dd1b6a236e8f8a
YueliMa <56177848+YueliMa@users.noreply.github.com>
2021-12-10 18:39:41 -0500

    Create cleaned data.R

---
+++ cleaned data.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(readxl)
+library(readr)
+final_df <- read_csv("~/Desktop/p8105 final/Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv")
+final_df = 
+  final_df%>%
+  janitor::clean_names()%>%
+  na.omit()
+view(final_df)
